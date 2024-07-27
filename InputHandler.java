5c9ebcc3cc4c1cc2ef42ad22b1f0b9ff2fe0b83e
manshus389 <manshus389@gmail.com>
2024-07-28 01:33:09 +0530

    adding implementation for user input

--- InputHandler.java
+++ InputHandler.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
 public class InputHandler {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public int getHealth(String playerName) {
+        System.out.println("Enter health for " + playerName + ":");
+        return scanner.nextInt();
+    }
+
+    public int getStrength(String playerName) {
+        System.out.println("Enter strength for " + playerName + ":");
+        return scanner.nextInt();
+    }
+
+    public int getAttack(String playerName) {
+        System.out.println("Enter attack for " + playerName + ":");
+        return scanner.nextInt();
+    }
+
+    public void close() {
+        scanner.close();
+    }
     
 }
