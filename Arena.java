d3f0607e24bc4165c2fb5b8ab57f0d55f5f114ec
manshus389 <manshus389@gmail.com>
2024-07-28 05:23:00 +0530

    bug fix for determineFirstAttacker function

--- Arena.java
+++ Arena.java
@@ -9,16 +9,37 @@ public class Arena {
 
     public void startMatch() {
         Dice dice = new Dice();
+
+        Player attacker = determineFirstAttacker();
+        Player defender = (attacker == player1) ? player2 : player1;
+
         while (player1.isAlive() && player2.isAlive()) {
-            player1.attack(player2, dice);
-            if (!player2.isAlive()) break;
-            player2.attack(player1, dice);
+            System.out.println(attacker + " attacks " + defender);
+            attacker.attack(defender, dice);
+            if (!defender.isAlive()) break;
+
+            // Swap roles
+            Player temp = attacker;
+            attacker = defender;
+            defender = temp;
         }
         displayMatchSummary();
     }
 
     private Player determineFirstAttacker() {
-        return (player1.getHealth() < player2.getHealth()) ? player1 : player2;
+        int roll1 = new Dice().roll();
+        int roll2 = new Dice().roll();
+        System.out.println("Player 1 rolls: " + roll1);
+        System.out.println("Player 2 rolls: " + roll2);
+
+        if (roll1 > roll2) {
+            return player1;
+        } else if (roll2 > roll1) {
+            return player2;
+        } else {
+            // In case of a tie, re-roll
+            return determineFirstAttacker();
+        }
     }
 
     private void displayMatchSummary() {
@@ -27,7 +48,7 @@ public class Arena {
         System.out.println("Player 1: " + player1);
         System.out.println("Player 2: " + player2);
 
-        // Determine and display the winner
+       
     if (player1.isAlive() && !player2.isAlive()) {
         System.out.println("The winner is Player1");
     } else if (player2.isAlive() && !player1.isAlive()) {
