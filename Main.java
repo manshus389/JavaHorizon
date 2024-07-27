78b4102bc992442e9bd2f65e3497b7fb7ba28773
manshus389 <manshus389@gmail.com>
2024-07-28 01:37:58 +0530

    changing Main file for user input

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Player playerA = new Player(50, 5, 10);
-        Player playerB = new Player(100, 10, 5);
+
+        InputHandler inputHandler = new InputHandler();
+
+        //taking input of player A
+        int healthA = inputHandler.getHealth("Player A");
+        int strengthA = inputHandler.getStrength("Player A");
+        int attackA = inputHandler.getAttack("Player A");
+
+        // taking input of Player B
+        int healthB = inputHandler.getHealth("Player B");
+        int strengthB = inputHandler.getStrength("Player B");
+        int attackB = inputHandler.getAttack("Player B");
+
+        Player playerA = new Player(healthA, strengthA, attackA);
+        Player playerB = new Player(healthB, strengthB, attackB);
+
 
         Arena arena = new Arena(playerA, playerB);
         arena.startMatch();
+
+        inputHandler.close();
     }
     
 }
